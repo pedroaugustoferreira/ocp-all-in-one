b3c0e906646daad4db1e85718f51d4a09da24030
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2019-12-02 13:41:42 -0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -7,18 +7,6 @@ gw="192.168.100.1"
 vm_path="OCP"
 
 
-vm_path_full=$(govc ls -L|grep vm)/$vm_path/$host_full
-
-
-
-echo $vm_path_full
-
-
-echo "govc vm.change -e=\"disk.enableUUID=1\" -vm='$vm_path_full'"|sh
-
-exit 1
-
-
 cat /root/.bashrc|egrep -v "GOVC" > /tmp/.bashrc
 echo "export GOVC_URL='192.168.100.102'"                  >> /tmp/.bashrc
 echo "export GOVC_USERNAME='administrator@vsphere.local'" >> /tmp/.bashrc
@@ -28,67 +16,94 @@ mv -f /tmp/.bashrc /root/.bashrc
 . /root/.bashrc
 
 
-echo "---- hostname "
-hostnamectl set-hostname $host_full
-hostname 
+conf_hostname()
+{
+	echo " "
+	echo "---- hostname"
+	hostnamectl set-hostname $host_full
+	hostname
+}
 
-echo " "
-echo "---- network"
-
-cat /etc/sysconfig/network-scripts/ifcfg-ens192 |egrep -v "BOOTPROTO|IPADDR|GATEWAY|NETMAKS|DNS1" > /tmp/network
-echo "IPADDR=$ip"            >> /tmp/network
-echo "GATEWAY=$gw"           >> /tmp/network
-echo "NETMAKS=255.255.255.0" >> /tmp/network
-echo "BOOTPROTO=static"      >> /tmp/network
-echo "DNS1=$gw"              >> /tmp/network
-
-cat /tmp/network
-mv -f /tmp/network /etc/sysconfig/network-scripts/ifcfg-ens192
-
-echo " "
-echo "---- /etc/hosts"
-echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
-echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
-echo "$ip $host_full $host" >> /etc/hosts
-
-echo " "
-echo "---- ssh key "
-rm -rf /root/.ssh/*
-cat /dev/zero | ssh-keygen -q -N ""
-sshpass -p "jakarosa" ssh-copy-id -o StrictHostKeyChecking=no root@ocp02.example.com
+conf_network()
+{
+	echo " "
+	echo "---- network"
+	cat /etc/sysconfig/network-scripts/ifcfg-ens192 |egrep -v "BOOTPROTO|IPADDR|GATEWAY|NETMAKS|DNS1" > /tmp/network
+	echo "IPADDR=$ip"            >> /tmp/network
+	echo "GATEWAY=$gw"           >> /tmp/network
+	echo "NETMAKS=255.255.255.0" >> /tmp/network
+	echo "BOOTPROTO=static"      >> /tmp/network
+	echo "DNS1=$gw"              >> /tmp/network
+	cat /tmp/network
+	mv -f /tmp/network /etc/sysconfig/network-scripts/ifcfg-ens192
+}
 
 
-echo " "
-echo "----- yum"
-#    8  subscription-manager register
-#    9  subscription-manager refresh
-#   10  subscription-manager list --available --matches '*OpenShift*'
-#   11  subscription-manager attch --pool=8a85f99b6e12cc13016e3baad78f45f0
-#   12  subscription-manager attach --pool=8a85f99b6e12cc13016e3baad78f45f0
-#   13  subscription-manager repos --disable="*"
 
-yum -y install atomic-openshift-clients openshift-ansible
-yum -y update
-yum install -y wget
-chmod +x jq-linux64
-mv -f jq-linux64 /usr/bin/jq
-curl -LO https://github.com/vmware/govmomi/releases/download/v0.15.0/govc_linux_amd64.gz
-gunzip -f govc_linux_amd64.gz
-chmod +x govc_linux_amd64
-mv -f govc_linux_amd64 /usr/bin/govc
+conf_hosts()
+{
+	echo " "
+	echo "---- /etc/hosts"
+	echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
+	echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
+	echo "$ip $host_full $host" >> /etc/hosts
+}
 
-#[root@ocp ~]# cat .bashrc
-#
-#export GOVC_URL='192.168.100.102'
-#export GOVC_USERNAME='administrator@vsphere.local'
-#export GOVC_PASSWORD='W@ster123'
-#export GOVC_INSECURE=1
-##
-# $govc vm.change -e="disk.enableUUID=1" -vm='VM Path'
 
-govc vm.change -e="disk.enableUUID=1" -vm="$(govc ls -L $(govc ls -L|grep vm)/$vm_path/$host_full)"
 
-govc ls -L
+conf_sshkey()
+{
+	echo " "
+	echo "---- ssh key "
+	rm -rf /root/.ssh/*
+	cat /dev/zero | ssh-keygen -q -N ""
+	sshpass -p "jakarosa" ssh-copy-id -o StrictHostKeyChecking=no root@ocp02.example.com
+}
+
+conf_yum()
+{
+	echo " "
+	echo "----- yum"
+	#    8  subscription-manager register
+	#    9  subscription-manager refresh
+	#   10  subscription-manager list --available --matches '*OpenShift*'
+	#   11  subscription-manager attch --pool=8a85f99b6e12cc13016e3baad78f45f0
+	#   12  subscription-manager attach --pool=8a85f99b6e12cc13016e3baad78f45f0
+	#   13  subscription-manager repos --disable="*"
+	yum -y install atomic-openshift-clients openshift-ansible
+	yum -y update
+	yum install -y wget
+}
+
+conf_govc()
+{
+	curl -LO https://github.com/vmware/govmomi/releases/download/v0.15.0/govc_linux_amd64.gz
+	gunzip -f govc_linux_amd64.gz
+	chmod +x govc_linux_amd64
+	mv -f govc_linux_amd64 /usr/bin/govc
+	
+	#[root@ocp ~]# cat .bashrc
+	#
+	#export GOVC_URL='192.168.100.102'
+	#export GOVC_USERNAME='administrator@vsphere.local'
+	#export GOVC_PASSWORD='W@ster123'
+	#export GOVC_INSECURE=1
+	##
+	# $govc vm.change -e="disk.enableUUID=1" -vm='VM Path'
+	
+	vm_path_full=$(govc ls -L|grep vm)/$vm_path/$host_full
+	echo $vm_path_full
+	echo "govc vm.change -e=\"disk.enableUUID=1\" -vm='$vm_path_full'"|sh
+	govc ls -L
+}
+
+conf_hostname
+conf_network
+conf_hosts
+conf_sshkey
+conf_yum
+conf_govc
+
 
 
 systemctl restart network
