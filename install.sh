b9fbb5fd1fba9d7da936ca0aecf3c2b30c1c0f02
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2019-12-02 17:06:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ install()
 {
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml;
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml;
+	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-monitoring/config.yml 
 }
 
 time install
