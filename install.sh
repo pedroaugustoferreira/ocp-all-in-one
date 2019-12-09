608ac5ee0c23416b2a6420afa76aa5111294e942
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2019-12-09 10:01:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,29 +9,39 @@ install()
 	echo "---------- deploy_cluster - Finish"
 
 	sleep 360
-
+	echo "-- deploy_cluster"      > pods.log
+	oc get pods --all-namespaces >> pods.log
+	
 	echo "---------- openshift-monitoring - Start"
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-monitoring/config.yml 
 	echo "---------- openshift-monitoring - Finish "
 
         sleep 360
+        echo "-- openshift-monitoring" > pods.log
+        oc get pods --all-namespaces  >> pods.log
 
 	echo "---------- metrics-server - Start"
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/metrics-server/config.yml
         echo "---------- metrics-server - Finish "
 
         sleep 360
+        echo "-- metrics-server"       > pods.log
+        oc get pods --all-namespaces  >> pods.log
 
 	echo "---------- openshift-metrics - Start "
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-metrics/config.yml
         echo "---------- openshift-metrics - Finish "
 
         sleep 360
+        echo "-- openshift-metrics"    > pods.log
+        oc get pods --all-namespaces  >> pods.log
 
 	echo "---------- openshift-logging - Start "
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-logging/config.yml
 	echo "---------- openshift-logging - Finish "
 
+        echo "-- openshift-logging"    > pods.log
+        oc get pods --all-namespaces  >> pods.log
 	
 	
 }
