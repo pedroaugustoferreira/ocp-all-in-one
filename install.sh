ecdd4518550186f49bbe79ef8f0314fb3caa2c3c
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2019-12-09 09:53:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,37 @@
 
 install()
 {
-	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml;
+#	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml;
+
+	echo "---------- deploy_cluster - Start "
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml;
+	echo "---------- deploy_cluster - Finish"
+
+	sleep 360
+
+	echo "---------- openshift-monitoring - Start"
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-monitoring/config.yml 
+	echo "---------- openshift-monitoring - Finish "
+
+        sleep 360
+
+	echo "---------- metrics-server - Start"
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/metrics-server/config.yml
+        echo "---------- metrics-server - Finish "
+
+        sleep 360
+
+	echo "---------- openshift-metrics - Start "
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-metrics/config.yml
+        echo "---------- openshift-metrics - Finish "
+
+        sleep 360
+
+	echo "---------- openshift-logging - Start "
 	ansible-playbook -i inventario /usr/share/ansible/openshift-ansible/playbooks/openshift-logging/config.yml
+	echo "---------- openshift-logging - Finish "
+
+	
 	
 }
 
