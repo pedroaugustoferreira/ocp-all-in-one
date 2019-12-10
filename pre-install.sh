f5651869650cc5674f3d6a94552e55f19760eada
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2019-12-10 09:16:29 -0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -47,7 +47,7 @@ conf_hosts()
 	echo "---- /etc/hosts"
 	echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
 	echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
-	echo "$ip $host_full $host apps.example.com api.example.com console.example.com metrics.example.com" >> /etc/hosts
+	echo "$ip $host_full $host apps.example.com api.example.com console.example.com metrics.example.com kibana.example.com" >> /etc/hosts
 }
 
 
